247b2231f762489c7148de972c945794d8ad8ba4
Nikita <sinyaev.ns@gmail.com>
2017-09-26 15:07:24 +0300

    lab1.c clean

--- SRC/lab1.c
+++ SRC/lab1.c
@@ -1,52 +1,37 @@
 /****************************************************************************
 
-    simple.c - простейшая программа для учебного стенда SDK-1.1
-
-    (C) simple.c, Ключев А.О.  2007 г.
-
-Это свободная программа; вы можете повторно распространять ее и/или
-модифицировать ее в соответствии с Универсальной Общественной
-Лицензией GNU, опубликованной Фондом Свободного ПО; либо версии 2,
-либо (по вашему выбору) любой более поздней версии.
-
-Эта программа распространяется в надежде, что она будет полезной,
-но БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ; даже без подразумеваемых гарантий
-КОММЕРЧЕСКОЙ ЦЕННОСТИ или ПРИГОДНОСТИ ДЛЯ КОНКРЕТНОЙ ЦЕЛИ.  Для
-получения подробных сведений смотрите Универсальную Общественную
-Лицензию GNU.
-
-Вы должны были получить копию Универсальной Общественной Лицензии
-GNU вместе с этой программой; если нет, напишите по адресу: Free
-Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-02111-1307 USA
-
-----------------------------------------------------------------------------
-Россия, Санкт-Петербург, кафедра вычислительной техники СПбГУИТМО 
-e-mail: kluchev@d1.ifmo.ru
+Проект:		Лаб. работа 1
+Название:	Дискретные порты ввода-вывода
+Файл:		lab1.c
+Авторы:		Романов И.Л., Синяев. Н.С.
+Описание:	Генерация требуемой анимации
 
 ****************************************************************************/
 #include "aduc812.h"
 #include "led.h"
 #include "max.h"
 
-#define ROR(x) (x)>>=1; (x)|=0x80;
-#define ROL(x) (x)=1; (x)|=0x80;
-
 static const unsigned char LAB_MAGIC = 0x77;
 static const unsigned char ANIM_DELAY = 700;
 
+/*
+Generates delay of execution
+INPUT:
+	ms - delay length in milliseconds
+*/
 void delay ( unsigned long ms )
 {
-volatile unsigned long i, j;
+	volatile unsigned long i, j;
 
     for( j = 0; j < ms; j++ )
     {
         for( i = 0; i < 50; i++ );
     }
-
-    
 }
+
+
 /*
+Anim() logic explanation:
 i   v           summand     
     00000000    00000011
 1   00000011    00000011    v += summand
@@ -59,12 +44,15 @@ i   v           summand
     11000011    00001100    v += 0x80
     etc
 */
-void anim_n( void ) {
+/*
+Performs requested animation
+*/
+void anim( void ) {
     unsigned char v = 0x00;
     unsigned char summand = 0x03;
     unsigned char i = 0;
     
-    for (i = 0; i < 4; i++) {
+    for ( i = 0; i < 4; i++ ) {
         v += summand;
         summand <<=1;
         v >>=1;        
@@ -73,7 +61,7 @@ void anim_n( void ) {
         delay(ANIM_DELAY);
     }
     
-    for (i = 0; i < 4; i++) {
+    for ( i = 0; i < 4; i++ ) {
         v -= summand;
         summand <<=1;
         v >>=1;        
@@ -85,31 +73,6 @@ void anim_n( void ) {
     }
 }
 
-/*void anim_n( void ) {
-    unsigned char v = 0x81;
-    unsigned char summand = 0x06;
-    unsigned char i = 0;
-    
-    for (i = 0; i < 3; i++) {
-        leds(v);
-        delay(ANIM_DELAY);
-        v += summand;
-        summand <<=1;
-        v >>=1;        
-        v += 0x80;
-    }
-    
-    for (i = 0; i < 4; i++) {
-        leds(v);
-        delay(ANIM_DELAY);
-        v -= summand;
-        summand <<=1;
-        v >>=1;
-        v += 0x80;
-    }
-    leds(0);
-    delay(ANIM_DELAY);    
-}*/
 
 void main( void )
 {
@@ -118,24 +81,11 @@ void main( void )
         unsigned char dip_value = read_max(EXT_LO);
         if ( LAB_MAGIC == dip_value )
         {
-            unsigned char left = 80;
-            
-            anim_n();
-            //leds(left);
-            //delay
-            /*
-            leds(0xDD);
-            delay(666);
-            leds(0xFF);
-            delay(666);
-            leds(0x11);
-            delay(666);
-            */
+            anim();
         }
         else 
         {
             leds(dip_value);
         }
-    
     }
-}    
+}
